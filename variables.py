f38116bf6ed53210828864feca08d6bd1eb7eecc
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-06-08 14:33:16 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Python es NO tipado
+name = "Mariel"
+age = 20
+is_single = True
+print(name)
+print(age)
+print(is_single)
+
+"""
+Hola, soy Mariel, tengo 20 años y soltera = True
+"""
+# Utilizando format String
+print("Hola soy {}, tengo {} y soltera = {}". format(name, age, is_single))
+
+print("Hola soy { name }, tengo { age } y soltera = { is_single }")
