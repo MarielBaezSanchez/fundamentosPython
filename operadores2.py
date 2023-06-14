3021fcd2cf68dd228058557c0e66eb0baf5a1a06
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-06-14 12:36:14 -0600

    05 Operadores Lógicos y Booleanos

---
+++ operadores2.py
@@ -0,0 +1,24 @@
+# Operadores Booleanos
+print("Operadores Booleanos")
+# < <=
+print(10 < 10)
+print(10 <= 10)
+# > >=
+print(10 > 10)
+print(10 >= 10)
+#==
+print(10 == 10)
+#!=
+print(10 != 10) 
+
+# Operadores Lógicos
+print("Operadores Lógicos")
+# and (True if all true)
+print(True and True)
+print(True and False)
+# or (True if at least one True)
+print(False or False)
+print(False or True)
+# not True
+print(not True)
+print(not False)
